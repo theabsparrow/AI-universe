5c5e4f8d40b5d759afd6d160d107ca658eee729c
theabsparrow <absbashar04@gmail.com>
2024-03-15 06:10:41 +0600

    final shot

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const showApiData = (loopData, isSeeMore) => {
     }
 
     loopData.forEach(singleData => {
-
+        // console.log(singleData);
         const div = document.createElement("div");
         div.classList = "border-[#11111119] border-[1px] p-6 rounded-lg"
         div.innerHTML = `
@@ -30,9 +30,9 @@ const showApiData = (loopData, isSeeMore) => {
     <div class="border-b-[1px] border-[#11111133] pb-6">
         <h1 class="mb-4 text-black font-semibold text-[25px] leading-[29px]">Features</h1>
         <div class="text-[#585858] space-y-3 leading-[26px]">
-            <p>1.Natural language processing</p>
-            <p>2.Contextual understanding</p>
-            <p>3.Text generation</p>
+            <p><span> 1 </span> ${singleData?.features[0] || 'not available'}</p>
+            <p><span> 2 </span> ${singleData?.features[1] || 'not available'}</p>
+            <p><span> 3 </span> ${singleData?.features[2] || 'not available'}</p>           
         </div>
     </div>
     <div class="mt-6 flex justify-between items-center">
@@ -64,12 +64,13 @@ const showTheDetail = async (id) => {
 
 const showWithModal = detail => {
     console.log(detail);
+    const accuracy = `${detail.accuracy.score}`* 100;
     const modalDetailBox = document.getElementById("modal-detail-box");
     modalDetailBox.innerHTML = `
     <form class="absolute -top-3 -right-4" method="dialog">
     <button class="btn btn-sm btn-circle bg-red-500 border-none text-white">✕</button>
 </form>
-<div class="flex flex-col lg:flex-row gap-4">
+<div class="flex flex-col-reverse lg:flex-row gap-4">
     <!-- left side -->
     <div class="border-[1px] border-[#EB5757] bg-[#EB57570D] p-2 lg:p-[30px] rounded-lg">
         <h1
@@ -93,18 +94,18 @@ const showWithModal = detail => {
                 <h1 class="text-black text-sm lg:text-[25px] font-semibold leading-[35px]">Features
                 </h1>
                 <ol type="dotted" class="text-[#585858] leading-[26px]">
-                    <li>${detail.features["1"].feature_name}</li>
-                    <li>${detail.features["2"].feature_name}</li>
-                    <li>${detail.features["3"].feature_name}</li>
+                    <li>${detail.features["1"]?.feature_name || 'not available'}</li>
+                    <li>${detail.features["2"]?.feature_name || 'not available'}</li>
+                    <li>${detail.features["3"]?.feature_name || 'not available'}</li>
                 </ol>
             </div>
             <div>
                 <h1 class="text-black text-sm lg:text-[25px] font-semibold leading-[35px]">Integrations
                 </h1>
                 <ol class="text-[#585858] leading-[26px]">
-                    <li>${detail.integrations[0]}</li>
-                    <li>${detail.integrations[1]}</li>
-                    <li>${detail.integrations[2]}</li>
+                    <li>${detail?.integrations[0] || 'not available'}</li>
+                    <li>${detail?.integrations[1] || 'not available'}</li>
+                    <li>${detail?.integrations[2] || 'not available'}</li>
                 </ol>
             </div>
         </div>
@@ -113,8 +114,9 @@ const showWithModal = detail => {
     <!-- right side -->
 
     <div class="border-[1px] border-[#E7E7E7] bg-white p-2 lg:p-[30px] rounded-lg  lg:w-[50vw]">
-        <div class="flex justify-center">
-            <img class="h-[25vh] lg:h-auto" src="${detail.image_link[0]}" alt="modal-image">
+        <div class="flex justify-center relative">
+            <img class="h-[25vh] lg:h-auto rounded-lg" src="${detail?.image_link[0] || detail.image_link[1]}" alt="modal-image">
+            <h1 class="text-white bg-button-color px-[15px] py-[5px] font-semibold leading-[19px] rounded-lg absolute right-1">${accuracy} <span> % accuracy </span> </h1>
         </div>
         <div class="mt-3 lg:mt-6">
             <h1 class="text-black text-xl lg:text-[25px] font-semibold leading-4 lg:leading-[35px] mb-2 lg:mb-4"> ${detail.input_output_examples
